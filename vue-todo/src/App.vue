8275e0ec6ad04bf8956f02d294bed5a0cd313f5a
jimin <studyingJM2123@gmail.com>
2021-08-07 04:01:01 +0900

    Todo CRUD - Read

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,23 +1,51 @@
 <template>
   <div>
-    <h1>Vue Todo with TypeScript</h1>
-    <TodoInput
-      :item="todoText"
-      @input="updateTodoText"
-      @add="addTodoItem"
-    ></TodoInput>
+    <header>
+      <h1>Vue Todo with TypeScript</h1>
+    </header>
+    <main>
+      <TodoInput
+        :item="todoText"
+        @input="updateTodoText"
+        @add="addTodoItem"
+      ></TodoInput>
+      <div>
+        <ul>
+          <TodoListItem
+            v-for="(todoItem, index) in todoItems"
+            :key="index"
+            :todoItem="todoItem"
+          ></TodoListItem>
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TodoInput from "./components/TodoInput.vue";
+import TodoListItem from "./components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItems: any[]) {
+    const parsed = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+  fetch() {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
 
 export default Vue.extend({
-  components: { TodoInput },
+  components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "",
+      todoItems: [] as any[],
     };
   },
   methods: {
@@ -26,13 +54,24 @@ export default Vue.extend({
     },
     addTodoItem() {
       const value = this.todoText;
-      localStorage.setItem(value, value); // (key , value)를 같은 형태로 저장
+      if (value.length <= 0) {
+        return;
+      }
+      this.todoItems.push(value);
+      storage.save(this.todoItems);
+      // localStorage.setItem(value, value);
       this.initTodoText();
     },
     // 텍스트 초기화를 위한 함수
     initTodoText() {
       this.todoText = "";
     },
+    fetchTodoItems() {
+      this.todoItems = storage.fetch();
+    },
+  },
+  created() {
+    this.fetchTodoItems();
   },
 });
 </script>
